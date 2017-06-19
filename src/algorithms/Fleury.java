340b1f74d8b17417ba2c205aa4f5a176f8567165
Timo Peters <timo.peters@haw-hamburg.de>
2017-06-19 10:11:19 +0200

    Fleury

---
+++ src/algorithms/Fleury.java
@@ -0,0 +1,59 @@
+package algorithms;
+
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.List;
+
+import org.graphstream.graph.Edge;
+import org.graphstream.graph.Graph;
+import org.graphstream.graph.Node;
+
+public class Fleury {
+
+	public static List<Node> getPath(Graph eulerKreisPositiv2) {
+		List<Node> eulerPath = new ArrayList<>();
+		// Zufälligen Startknoten finden
+		List<Node> nodeList = new ArrayList<>(eulerKreisPositiv2.getNodeSet());
+		int randomIndex = (int) (Math.random() * nodeList.size() - 1);
+		Node start = nodeList.get(randomIndex);
+		eulerPath.add(start);
+
+		Node v = start;
+		Iterator<Edge> neighbors = v.getEdgeIterator();
+		while (eulerKreisPositiv2.getEdgeSet().size() > 0) {
+			while (neighbors.hasNext()) {
+				Edge nextEdge = neighbors.next();
+				Node s = nextEdge.getSourceNode();
+				Node t = nextEdge.getTargetNode();
+				if (t.getDegree() != 1) {
+					if (!isBridge(eulerKreisPositiv2, nextEdge)) {
+						eulerKreisPositiv2.removeEdge(nextEdge);
+						v = nextEdge.getTargetNode();
+						break;
+					}
+				} else {
+					eulerPath.add(t);
+					return eulerPath;
+				}
+			}
+		}
+
+		return eulerPath;
+	}
+
+	private static boolean isBridge(Graph graph, Edge edge) {
+		Node s = edge.getSourceNode();
+		Node t = edge.getTargetNode();
+
+		graph.removeEdge(edge);
+		if (BreadthFirstSearch.getSteps(s.toString(), t.toString(), graph) != -1) {
+			graph.addEdge(s.toString() + t.toString(), s, t);
+			return true;
+		} else {
+			graph.addEdge(s.toString() + t.toString(), s, t);
+			return false;
+		}
+
+	}
+
+}
